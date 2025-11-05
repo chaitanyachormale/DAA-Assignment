529fe5fe1c6b0f483e97ed74b4dd8626df110eda
chaitanyachormale <155938538+chaitanyachormale@users.noreply.github.com>
2025-11-05 14:15:30 +0530

    Update Knapsack.cpp

--- DAA/Knapsack.cpp
+++ DAA/Knapsack.cpp
@@ -2,16 +2,16 @@
 #include <vector>
 using namespace std;
 
-// Function to get the maximum of two numbers
+
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-// Function to solve 0/1 Knapsack problem
+
 int knapsack(int W, vector<int>& wt, vector<int>& val, int n) {
     vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
 
-    // Build table dp[][] in bottom-up manner
+  
     for (int i = 0; i <= n; i++) {
         for (int w = 0; w <= W; w++) {
             if (i == 0 || w == 0)
@@ -24,17 +24,18 @@ int knapsack(int W, vector<int>& wt, vector<int>& val, int n) {
         }
     }
 
-    return dp[n][W]; // Maximum value possible
+    return dp[n][W]; 
 }
 
 int main() {
-    // Minor change in input
-    vector<int> val = {70, 90, 120};   // item values
-    vector<int> wt  = {10, 25, 35};    // item weights
-    int W = 50;                        // Knapsack capacity
-    int n = val.size();               // Number of items
+    
+    vector<int> val = {70, 90, 120};   
+    vector<int> wt  = {10, 25, 35};   
+    int W = 50;                       
+    int n = val.size();               
 
     cout << "Maximum value in Knapsack = " << knapsack(W, wt, val, n) << endl;
 
     return 0;
 }
+
